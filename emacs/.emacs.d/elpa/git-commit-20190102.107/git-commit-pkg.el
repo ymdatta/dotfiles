e5bd3a58df11acab72495fa39d07a7441c8369c8
Mohana Datta Yelugoti <ymdatta.work@gmail.com>
2020-11-01 15:05:59 +0530

    Remove gitignore files already present in the repository

--- emacs/.emacs.d/elpa/git-commit-20190102.107/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190102.107" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "2e53d7babc666242e00625d7fe757f0c07e56789" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- emacs/.emacs.d/elpa/ir-black-theme-20130303.755/ir-black-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ir-black-theme" "20130303.755" "Port of ir-black theme" 'nil :commit "36e930d107604b5763c80294a6f92aaa02e6c272" :keywords '("faces") :authors '(("Jon-Michael Deldin" . "dev@jmdeldin.com")) :maintainer '("Jon-Michael Deldin" . "dev@jmdeldin.com"))
