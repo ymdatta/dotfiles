732a43e2dd2a33fa4b073b8a6c5bee9d4a624db3
Mohana Datta Yelugoti <ymdatta.work@gmail.com>
2021-01-26 13:42:44 +0530

    Add auto-package-update package
    This package provides functionality for automatically updating
    Emacs pacakges periodically.
    
    Link: https://github.com/rranelli/auto-package-update.el
    
    Signed-off-by: Mohana Datta Yelugoti <ymdatta.work@gmail.com>

--- emacs/.emacs.d/myinit.el
+++ emacs/.emacs.d/myinit.el
@@ -155,12 +155,12 @@
 (use-package docker-tramp
   :ensure t)
 
-;; (use-package auto-package-update
-;;   :ensure t
-;;   :config
-;;   (setq auto-package-update-delete-old-versions t)
-;;   (setq auto-package-update-hide-results t)
-;;   (auto-package-update-maybe))
+(use-package auto-package-update
+   :ensure t
+   :config
+   (setq auto-package-update-delete-old-versions t)
+   (setq auto-package-update-hide-results t)
+   (auto-package-update-maybe))
 
 ;; (use-package ggtags
 ;;    :ensure t
